100f98701cdae34368bd82c9e49d6d9e9a47fa5b
Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
2021-09-21 04:41:47 -0400

    initial color mode to dark

--- theme/index.ts
+++ theme/index.ts
@@ -1,11 +1,16 @@
-import { extendTheme } from '@chakra-ui/react';
+import { extendTheme, ThemeConfig } from '@chakra-ui/react';
 import '@fontsource/inter';
+
+const config: ThemeConfig = {
+  initialColorMode: 'dark'
+};
 
 const theme = {
   fonts: {
     body: 'Inter',
     heading: 'Inter'
-  }
+  },
+  config
 };
 
 export default extendTheme(theme);
