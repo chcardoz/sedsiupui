0dbdfa9ed16da1ab76efeeeda676b9eebc19fcac
Chris Cardoza <59742506+chcardoz@users.noreply.github.com>
2022-01-08 19:55:34 -0500

    dynamic import react pixi

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,17 +1,25 @@
 import { Box, Center, Divider, Stack } from '@chakra-ui/react';
+import dynamic from 'next/dynamic';
 import { Footer } from './Footer';
 import { Navbar } from './Navbar';
+
+const StarBackground = dynamic((() => import('./StarBackground')) as any, {
+  ssr: false
+});
 
 interface layoutProps {}
 
 export const Layout: React.FC<layoutProps> = ({ children }) => {
   return (
-    <Center paddingTop="8em">
-      <Stack maxW="4xl" direction="column">
-        <Box>{children}</Box>
-        <Divider />
-        <Footer />
-      </Stack>
-    </Center>
+    <>
+      <StarBackground />
+      <Center paddingTop="8em">
+        <Stack maxW="4xl" direction="column">
+          <Box>{children}</Box>
+          <Divider />
+          <Footer />
+        </Stack>
+      </Center>
+    </>
   );
 };
